3cfe8a0b56f403ad6027a1e417cd88d2f9b8c2b1
Petar Bozin <petar.bozin@md-softwaredesign.de>
2016-06-14 15:44:59 +0200

    Updated hello_obp

--- hello_obp.py
+++ hello_obp.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import sys, requests
+# Helper function to merge headers
+def merge(x, y):
+    z = x.copy()
+    z.update(y)
+    return z
 
 # Note: in order to use this example, you need to have at least one account
-# that you can send money from (i.e. be the owner).
 
 USERNAME     = 'robert.x.0.gh@example.com'
 PASSWORD     = '3e3a3102'
@@ -24,7 +28,7 @@ OUR_BANK = 'obp-bank-x-gh'
 # You probably don't need to change those
 loginHeader  = { 'Authorization' : 'DirectLogin username="%s",password="%s",consumer_key="%s"' % (USERNAME, PASSWORD, CONSUMER_KEY)}
 
-# login and receive authorized token
+# Login and receive authorized token
 print 'Login as {0} to {1}'.format(loginHeader, LOGIN_URL)
 r = requests.get(LOGIN_URL, headers=loginHeader)
 
@@ -32,39 +36,39 @@ if (r.status_code != 200):
     print "error: could not login"
     sys.exit(0)
 
-# login ok - create authorization headers
+# Login OK - create authorization headers
 token = r.json()['token']
 
 print "Received token: {0}".format(token)
 
+# Prepare headers
 directlogin  = { 'Authorization' : 'DirectLogin token=%s' % token}
 content_json = { 'content-type'  : 'application/json' }
 limit        = { 'obp_limit'     : '25' }
 
-def merge(x, y):
-    z = x.copy()
-    z.update(y)
-    return z
-
-
+# Get all private accounts for this user
 response = requests.get(u"{0}/obp/v1.4.0/banks/{1}/accounts/private".format(BASE_URL, OUR_BANK), headers=directlogin)
 print response.status_code
-#print response.text
 
+# Print accounts 
 accounts = response.json()['accounts']
 for a in accounts:
     print a['id']
 
-#just picking first account
+# Just picking first account
 our_account = accounts[0]['id']
 print "our account: {0}".format(our_account)
 
+# Prepare post data and set new label value
 post_data = {
     'id' : '%s' % our_account,
     'label' : 'New label',
     'bank_id': '%s' % OUR_BANK 
 }
 
+# Send post request with attached json with new label value
 response = requests.post(u"{0}/obp/v1.4.0/banks/{1}/accounts/{2}".format(BASE_URL, OUR_BANK, our_account), json=post_data, headers=merge(directlogin, content_json))
 print response.status_code
+
+# Print result
 print response.text
